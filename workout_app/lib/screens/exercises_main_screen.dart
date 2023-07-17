36e033c8a435475a56d55ef7d3e1f2b267632c36
OluC94 <coker.olu94@gmail.com>
2023-07-17 17:52:39 +0100

    create placeholder exercises page

--- workout_app/lib/screens/exercises_main_screen.dart
+++ workout_app/lib/screens/exercises_main_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:workout_app/components/app_bar.dart';
+import 'package:workout_app/components/custom_search_form.dart';
+import 'package:workout_app/components/main_button.dart';
+
+import '../utils/style_variables.dart';
 
 class ExercisesScreen extends StatefulWidget {
   const ExercisesScreen({super.key});
@@ -10,8 +15,39 @@ class ExercisesScreen extends StatefulWidget {
 class _ExercisesScreenState extends State<ExercisesScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Exercises")),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: const CustomAppBar(
+        title: 'Exercises',
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: MainButton(
+              foregroundColor: Colors.white,
+              backgroundColor: mainThemeColour,
+              borderColor: Colors.grey,
+              text: 'Add exercise',
+              boxWidth: mainButtonSize,
+              boxHeight: mainRectButtonHeight,
+            ),
+          ),
+          const CustomSearchForm(),
+          Container(
+            height: 350,
+            width: 350,
+            padding: const EdgeInsets.all(25),
+            margin: const EdgeInsets.all(25),
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: const [Text('Exercises placeholder')],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
